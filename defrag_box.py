9818c2b7b4b1124bbca422ca9a50b2054c88f51f
Oliver Ruoff <oliver93ruoff@web.de>
2020-10-10 11:54:20 +0200

    formatted defrag_box.py

--- defrag_box.py
+++ defrag_box.py
@@ -8,11 +8,12 @@ from datetime import datetime as dt
 import dropbox
 from dotenv import load_dotenv
 
-#loading .env file
+# loading .env file
 load_dotenv()
 
 FILE_NAME_ENDINGS_TO_FILTER_ON = ['jpg', 'jpeg', 'png', 'mp4', 'avi']
 BLOCK_LIST = ['WA']
+
 
 def init_dropbox(dropbox_token: str):
     return dropbox.Dropbox(dropbox_token)
@@ -26,11 +27,14 @@ def load_all_files(dbx: dropbox.Dropbox):
         entries += response.entries
     return entries
 
+
 def filter_files_on_name_ending(files: list, name_endings: list):
     return [_file for _file in files if any([_file.name.endswith(ending) for ending in name_endings])]
 
+
 def filter_files_on_name_contains_any_from_block_list(files: list, block_list):
     return [_file for _file in files if not any([blocked_word in _file.name for blocked_word in block_list])]
+
 
 def get_relocationpath_for_file(file_metadata, to_path_prefix='/DefragBox'):
 
@@ -43,23 +47,24 @@ def get_relocationpath_for_file(file_metadata, to_path_prefix='/DefragBox'):
     _to_path = '/'.join([to_path_prefix, year, month, day, _new_file_name])
     return dropbox.files.RelocationPath(from_path=file_metadata.path_lower, to_path=_to_path)
 
+
 if __name__ == '__main__':
     dbx = init_dropbox(os.getenv('DROPBOX_TOKEN'))
     print(dt.now(), 'Loading all files. This will take a while (Depending on the amount of files in your dropbox).')
     unfiltered_files = load_all_files(dbx)
     print(dt.now(), 'Found', len(unfiltered_files), 'files.')
-    name_endings = FILE_NAME_ENDINGS_TO_FILTER_ON + [ending.upper() for ending in FILE_NAME_ENDINGS_TO_FILTER_ON]
+    name_endings = FILE_NAME_ENDINGS_TO_FILTER_ON + \
+        [ending.upper() for ending in FILE_NAME_ENDINGS_TO_FILTER_ON]
     print(dt.now(), 'Filtering files on endings:', name_endings)
     image_files = filter_files_on_name_ending(unfiltered_files, name_endings)
     print(dt.now(), 'Found', len(image_files), 'image files.')
     print(dt.now(), 'Filtering files on block list words:', BLOCK_LIST)
-    image_files = filter_files_on_name_contains_any_from_block_list(image_files, BLOCK_LIST)
+    image_files = filter_files_on_name_contains_any_from_block_list(
+        image_files, BLOCK_LIST)
     print(dt.now(), 'Remaining files:', len(image_files))
-    relocation_paths = [get_relocationpath_for_file(img_file) for \
-        img_file in image_files if type(img_file) == dropbox.files.FileMetadata]
+    relocation_paths = [get_relocationpath_for_file(img_file) for
+                        img_file in image_files if type(img_file) == dropbox.files.FileMetadata]
     sample = relocation_paths[0] if len(relocation_paths) > 0 else None
     print(dt.now(), 'Batch copying the files to sorted folders. 1st sample:', sample)
     dbx.files_copy_batch(relocation_paths)
     print(dt.now(), 'Done!')
-
-
